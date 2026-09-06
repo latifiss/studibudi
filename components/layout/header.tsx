cfa19179cfbb9a0f72abe4feb8b1d356b1f4e656
Latif Issaka <83374530+latifiss@users.noreply.github.com>
2026-09-06 21:05:16 +0000

    Fix Get Started route to signin

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -57,7 +57,7 @@ const Header = () => {
   }, [authenticated])
 
   const handleGetStarted = () => {
-    router.push('/get-started')
+    router.push('/signin')
     setIsMenuOpen(false)
   }
 
@@ -172,7 +172,6 @@ const Header = () => {
             </button>
           )}
         </div>
-      </header>
 
       {isMenuOpen && !authenticated && (
         <div className="sm:hidden fixed top-18 left-0 right-0 bg-white border-b border-border z-40 px-4 py-6 shadow-lg">
